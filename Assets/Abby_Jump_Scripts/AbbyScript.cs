dd14f872c01eaf98cd87b4dd6d5ed482a630c5da
Frostishyper <frostplays03@gmail.com>
2025-08-25 14:24:46 +0800

    Main Core Game Complete

--- Assets/Abby_Jump_Scripts/AbbyScript.cs
+++ Assets/Abby_Jump_Scripts/AbbyScript.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class AbbyScript : MonoBehaviour
 {
     public Rigidbody2D AbbyBody;
+    public CoreScript Core;
     public float JumpPower = 6.5f;
+    public bool AbbyIsAlive = true;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.name = "Abby";
-        
+        Core = GameObject.FindGameObjectWithTag("CoreTag").GetComponent<CoreScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Jump Behavior
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && AbbyIsAlive)
         {
             AbbyBody.velocity = Vector2.up * JumpPower;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Core.GameOver();
     }
 }
--- Assets/Abby_Jump_Scripts/CoreScript.cs
+++ Assets/Abby_Jump_Scripts/CoreScript.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CoreScript : MonoBehaviour
 {
     public int PlayerScore;
     public Text ScoreText;
+    public GameObject GameOver_Screen;
 
     public void AddScore(int ScoreToAdd)
     {
         PlayerScore = PlayerScore + ScoreToAdd;
         ScoreText.text = PlayerScore.ToString();
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        GameOver_Screen.SetActive(true);
     }
 }
